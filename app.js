f41655846ec275cc93ad257bbcf52d655257e04b
ibrohim-abdurahimov <ibr20040322@gmail.com>
2024-08-30 07:51:24 +0500

    masala

---
+++ app.js
@@ -0,0 +1,200 @@
+{
+    // console.log("Masala-1")
+    // let days= 5
+    // switch(days){
+    //     case 1 :
+    //         console.log("Dushanba")
+    //         break
+    //     case 2 :
+    //         console.log("Seshanba")
+    //         break
+    //     case 3 :
+    //         console.log("Chorshanba")
+    //         break
+    //     case 4 :
+    //         console.log("Payshanba")
+    //         break
+    //     case 5 :
+    //         console.log("Juma")
+    //         break
+    //     case 6 :
+    //         console.log("Shanba")
+    //         break
+    //     case 7 :
+    //         console.log("Yakshanba")
+    //         break
+    //         default:
+    //             console.log("1-7 gacha son kiriting")
+       
+    // }
+}
+{
+    // console.log("Masala-2")
+    // let k =4
+    // switch(k){
+    //     case 1 :
+    //         console.log("Yomon")
+    //         break
+    //     case 2 :
+    //         console.log("Qoniqarsiz")
+    //         break
+    //     case 3 :
+    //         console.log("Qoniqarli")
+    //         break
+    //     case 4 :
+    //         console.log("Yashi")
+    //         break
+    //     case 5 :
+    //         console.log("A'lo")
+    //         break
+    //         default:
+    //             console.log("Xato")
+    // }
+}
+
+{
+    // console.log("Masala-3")
+    // let month = 3
+    // switch(month){
+    //     case 1 :
+    //     case 2 :
+    //     case 12 :
+    //         console.log("Qish")
+    //         break
+    //     case 3 :
+    //     case 4 :
+    //     case 5 :
+    //         console.log("Bahor")
+    //         break
+    //     case 6 :
+    //     case 7 :
+    //     case 8 :
+    //         console.log("Yoz")
+    //         break
+    //     case 9 :
+    //     case 10 :
+    //     case 11 :
+    //         console.log("Kuz")
+    //         break
+    //         default:
+    //             console.log("Xato")
+    // }
+}
+
+{
+    // console.log("Masala-4")
+    // let days = 9
+    // console.log(days)
+    // switch(days){
+    //     case 1 :
+    //     case 3 :
+    //     case 5 :
+    //     case 7 :
+    //     case 8 :
+    //     case 10 :
+    //     case 12 :
+    //         console.log("31 kun bor")
+    //         break
+    //     case 4 :
+    //     case 6 :
+    //     case 9 :
+    //     case 11 :
+    //         console.log("30 kun bor")
+    //         break
+    //     case 2 :
+    //         console.log("29 kun bor")
+    //         break
+    //         default:
+    //             console.log("Xato")
+        
+    // }
+}
+
+{
+    // console.log("Masala-5")
+    // let a = 5
+    // let b = 5
+    // let amal = 1
+    // switch(amal){
+    //     case 1 :
+    //         console.log(a+b)
+    //         break
+    //     case 2 :
+    //         console.log(a-b)
+    //         break
+    //     case 3 :
+    //         console.log(a/b)
+    //         break
+    //     case 4 :
+    //         console.log(a*b)
+    //         break
+    //         default:
+    //             console.log("Xato")
+    // }
+}
+
+{
+    // console.log("Masala-6")
+    // let uzunlik = 200
+    // let amal = 5    
+    // switch(amal){
+    //     case 1 :
+    //         console.log(`${uzunlik}dm / ${10} m = ${uzunlik/10}m`)
+    //         break
+    //     case 2 :
+    //         console.log(`${uzunlik}km * ${1000} m = ${uzunlik*1000}m`)
+    //         break
+    //     case 3 :
+    //         console.log(`${uzunlik}m`)
+    //         break
+    //     case 4 :
+    //         console.log(`${uzunlik}mm / ${1000} m = ${uzunlik/1000}m`)
+    //         break
+    //     case 5 :
+    //         console.log(`${uzunlik}sm / ${100} m = ${uzunlik/100}m`)
+    //         break
+    //         default:
+    //             console.log("Xato")
+               
+    // }
+}
+{
+    // console.log("Masala-7")
+    // let ogirlik = 2
+    // let amal = 5    
+    // switch(amal){
+    //      case 1 :
+    //          console.log(`${ogirlik}kg`)
+    //          break
+    //      case 2 :
+    //          console.log(`${ogirlik} mg / ${1_000_000} kg = ${ogirlik/1_000_000}kg`)
+    //          break
+    //      case 3 :
+    //          console.log(`${ogirlik} g / ${1000} kg = ${ogirlik/1000}kg`)
+    //          break
+    //      case 4 :
+    //          console.log(`${ogirlik} t * ${1000} kg = ${ogirlik*1000}kg`)
+    //          break
+    //      case 5 :
+    //          console.log(`${ogirlik} sr * ${100} kg = ${ogirlik*100}kg`)
+    //          break
+    //          default:
+    //             console.log("Xato")
+    //  }
+}
+
+{
+    // console.log("Masala-8")
+    // let day = 30
+    // let month = 2
+    // switch(month){
+    //     case 2 :
+    //         console.log(`month ${month} day ${day}`)
+    //         break
+    // }
+
+}
+{
+    console.log("Masala-9")
+    
+}
